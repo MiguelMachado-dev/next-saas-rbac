2ad3a247b382e3770798fc5d87c70aeec8c0e92d
Miguel Machado <hello@miguelmachado.dev>
2025-10-18 18:18:11 -0300

    feat(auth): enhance permissions and ability definitions for user roles

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -23,6 +23,10 @@ const appAbilitiesSchema = z.union([
   z.tuple([z.literal('manage'), z.literal('all')]),
 ])
 
+export * from './models/organization'
+export * from './models/project'
+export * from './models/user'
+
 type AppAbilities = z.infer<typeof appAbilitiesSchema>
 
 export type AppAbility = MongoAbility<AppAbilities>
